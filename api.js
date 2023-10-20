f5489510252a37d3ea08e32387b9533f7fc02a47
Oscar Cáceres <ita190@hotmail.com>
2023-10-19 22:36:57 -0500

    Server Express, Mongo Conexion, y  algo mas

---
+++ api.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var TaskModel = require('./task_schema');
+var router = express.Router();
+
+
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
+const db = (query);
+
+mongoose.Promise = global.Promise;
+
+//mongoose.set('strictQuery', false);
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (error) {
+    if (error) {
+        console.log("Error!" + error);
+    } else {
+        console.log("Se ha conectado con la base de datos exitosamente");
+    }
+});
+
+module.exports = router;
