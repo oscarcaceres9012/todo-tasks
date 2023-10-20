13c547bdf87afd4a15b2a76bf0e8113b368d6640
Oscar Cáceres <ita190@hotmail.com>
2023-10-19 23:36:55 -0500

    CRUD OK

--- api.js
+++ api.js
@@ -37,4 +37,61 @@ mongoose.connect(db, {
     }
 });
 
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+    var newTask = new TaskModel(task);
+
+    newTask.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.get('/all-tasks', function (req, res) {
+    TaskModel.find(function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+        Name: req.body.Name,
+        Deadline: req.body.Deadline
+    }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
 module.exports = router;
